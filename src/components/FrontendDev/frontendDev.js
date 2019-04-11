f5ae9dad2cb72f283d5a8c0b41356f55604443bf
DTGillaird <dtg5088@gmail.com>
2019-04-11 19:06:54 -0400

    Trey helped me with the code for the check boxes, here is the commit

--- src/components/FrontendDev/frontendDev.js
+++ src/components/FrontendDev/frontendDev.js
@@ -1,99 +1,169 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import checkboxes from '../checkboxes/checkboxes';
-import Checkbox from '../Checkbox/checkbox';
-import { __values } from 'tslib';
+import React from "react";
 
-class Frontend extends React.Component {
+const skills = ["react", "angular", "css", "html"];
+
+class Form extends React.Component {
   constructor(props) {
     super(props);
-
     this.state = {
-      checkedItems: new Map(),
-      checkedBoxes: []
-    }
-
-    
-    this.handleChange = this.handleChange.bind(this);
-    //this.handleSubmit = this.handleSubmit.bind(this);
+      skills: {}
+    };
+    skills.forEach(skill => {
+      this.state.skills[skill] = false;
+    });
   }
 
+  submit = () => {
+    let arr = [];
+    Object.keys(this.state.skills).forEach(key => {
+      if (this.state.skills[key]) {
+        arr.push(key);
+      }
+    });
+    console.log("arr", arr.join(","));
+  };
 
-  handleChange(e) {
-    e.persist()
+  change = e => {
+    const { name } = e.target;
+    const skills = Object.assign({}, this.state.skills);
+    skills[name] = !skills[name];
+    console.log(skills);
 
-    const item = e.target.name;
-    const isChecked = e.target.checked;
-
-
-      // if(e.target.checked){
-      //   checkboxes.isChecked = true;
-      
-      // }else {
-      //   checkboxes.isChecked = false;
-  
-      // }
-
-      checkboxes.forEach( event =>{
-        console.log(event);
-        console.log(checkboxes.isChecked);
-      });
-
-
-    this.setState(prevState => ({ 
-
-
-      //[name]: values,
-      checkedItems: prevState.checkedItems.set(item, isChecked),
-      //checkboxes: prevState.checkboxes.set(item, status),
-      //checkedBoxes : !this.state.checkedBoxes.includes(e.target.name) && e.target.checked ? this.state.checkedBoxes.push(e.target.name) : this.state.checkedBoxes
-      //checkedBoxes : !this.state.ischecked ? this.state.checkedBoxes.push(e.target.name) : this.state.checkedBoxes
-    
-    }));
-    
-    
-    
-    console.log(e.target.name)
-    console.log(e.target.checked)
-    console.log(this.state.checkedBoxes)
-  }
-
-  onSubmit = e => {
-    e.preventDefault();
-    //this.props.onSubmit()
-    console.log(this.state);
     this.setState({
-        checkboxes: [],
-    })
-}
+      skills
+    });
+  };
+
+  swapSkills = () => {};
 
   render() {
-
-    
     return (
-      <form>
-      <React.Fragment>
-        {
-          checkboxes.map(item => (
-            <label key={item.key}>
-              <Checkbox 
-                name={item.name} 
-                checked={this.state.checkedItems.get(item.name)} 
-                onChange={this.handleChange} />
-              <span>{item.name}</span>
-            </label>
-          ))
-        }
-        <button onClick={() => this.onSubmit()}>Submit</button>
-      </React.Fragment>
-      </form>
-      
+      <div>
+        <button onClick={this.swapSkills}>Front End</button>
+        <button onClick={this.swapSkills}>BackEnd</button>
+        {Object.keys(this.state.skills).map(key => {
+          const isChecked = this.state.skills[key];
+          return (
+            <div>
+              <label>{key}</label>
+              <input
+                name={key}
+                type="checkbox"
+                checked={isChecked}
+                onChange={this.change}
+              />
+            </div>
+          );
+        })}
+        <button onClick={this.submit}>Submit</button>
+      </div>
     );
   }
 }
 
-export default Frontend;
+export default Form;
 
+
+
+
+
+
+// import React from 'react';
+// import PropTypes from 'prop-types';
+// import checkboxes from '../checkboxes/checkboxes';
+// import Checkbox from '../Checkbox/checkbox';
+// import { __values } from 'tslib';
+
+// class Frontend extends React.Component {
+//   constructor(props) {
+//     super(props);
+
+//     this.state = {
+//       checkedItems: new Map(),
+//       checkedBoxes: []
+//     }
+
+    
+//     this.handleChange = this.handleChange.bind(this);
+//     //this.handleSubmit = this.handleSubmit.bind(this);
+//   }
+
+
+//   handleChange(e) {
+//     e.persist()
+
+//     const item = e.target.name;
+//     const isChecked = e.target.checked;
+
+
+//       // if(e.target.checked){
+//       //   checkboxes.isChecked = true;
+      
+//       // }else {
+//       //   checkboxes.isChecked = false;
+  
+//       // }
+
+//       checkboxes.forEach( event =>{
+//         console.log(event);
+//         console.log(checkboxes.isChecked);
+//       });
+
+
+//     this.setState(prevState => ({ 
+
+
+//       //[name]: values,
+//       checkedItems: prevState.checkedItems.set(item, isChecked),
+//       //checkboxes: prevState.checkboxes.set(item, status),
+//       //checkedBoxes : !this.state.checkedBoxes.includes(e.target.name) && e.target.checked ? this.state.checkedBoxes.push(e.target.name) : this.state.checkedBoxes
+//       //checkedBoxes : !this.state.ischecked ? this.state.checkedBoxes.push(e.target.name) : this.state.checkedBoxes
+    
+//     }));
+    
+    
+    
+//     console.log(e.target.name)
+//     console.log(e.target.checked)
+//     console.log(this.state.checkedBoxes)
+//   }
+
+//   onSubmit = e => {
+//     e.preventDefault();
+//     //this.props.onSubmit()
+//     console.log(this.state);
+//     this.setState({
+//         checkboxes: [],
+//     })
+// }
+
+//   render() {
+
+    
+//     return (
+//       <form>
+//       <React.Fragment>
+//         {
+//           checkboxes.map(item => (
+//             <label key={item.key}>
+//               <Checkbox 
+//                 name={item.name} 
+//                 checked={this.state.checkedItems.get(item.name)} 
+//                 onChange={this.handleChange} />
+//               <span>{item.name}</span>
+//             </label>
+//           ))
+//         }
+//         <button onClick={() => this.onSubmit()}>Submit</button>
+//       </React.Fragment>
+//       </form>
+      
+//     );
+//   }
+// }
+
+// export default Frontend;
+///////////////////////////////////////////////////////////////////////////////////////////////
 // import React from 'react';
 // import Checkbox from "../Checkbox/checkbox"
 // // import "./style.css";
